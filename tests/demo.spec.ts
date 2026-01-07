aee837afe1b84707074bb4d4b03251298d85f7b9
Animesh66 <er.animesh6@hotmail.com>
2026-01-07 18:09:09 +1100

    Updated the code with correct details.

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -2,5 +2,5 @@ import {test, expect} from '@playwright/test';
 
 test('this is my first test', async ({page}) => {
     await page.goto('http://localhost:5173/home');
-    await expect(page).toHaveTitle('test');
+    await expect(page).toHaveTitle('learnwithanimesh');
 })
