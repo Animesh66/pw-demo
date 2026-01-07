63f1545fbc903ffc82acc46c626a4d7f17f7b4d9
Animesh66 <er.animesh6@hotmail.com>
2026-01-07 17:41:30 +1100

    Added the code to git.

---
+++ tests/demo.spec.ts
@@ -0,0 +1,6 @@
+import {test, expect} from '@playwright/test';
+
+test('this is my first test', async ({page}) => {
+    await page.goto('http://localhost:5173/home');
+    await expect(page).toHaveTitle('test');
+})
