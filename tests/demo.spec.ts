18c4091bd88087cc571f9a70139b299464c2fa2d
Animesh66 <er.animesh6@hotmail.com>
2026-01-10 22:17:07 +1100

    Updated the code with corect details.

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -3,4 +3,15 @@ import {test, expect} from '@playwright/test';
 test('this is my first test', async ({page}) => {
     await page.goto('http://localhost:5173/home');
     await expect(page).toHaveTitle('learnwithanimesh');
+    await page.locator('//input[@id="firstname"]').fill('Animesh');
+    await page.getByRole('button', { name: 'Single Click Me' }).click();
+    await page.getByText('Register Now').click();
+    await page.getByPlaceholder('Type Text').fill('Animesh');
+    await page.getByTestId('firstname').fill('Animesh');
+    await page.getByRole('button', {name: 'Home'}).nth(1).click();
+    console.log(await page.locator('.nav-links li').allTextContents());
+    const navLinks = page.locator('.nav-links li')
+    for (let element of await navLinks.elementHandles()) {
+        console.log(await element.textContent());
+    }
 })
