b83cce9b241258063f53c2b928c3172c2b844db3
Animesh66 <er.animesh6@hotmail.com>
2026-01-17 21:24:14 +1100

    Updated the status with correct details.

--- tests/playwright-assertions.spec.ts
+++ tests/playwright-assertions.spec.ts
@@ -3,5 +3,14 @@ import { test, expect } from '@playwright/test';
 
   test('Use Assertions in Playwright', async ({ page }) => {
     await page.goto('http://localhost:5173/');
+    // Page Assertion
     await expect(page).toHaveTitle(/learnwithanimesh/);
+    await expect(page).toHaveURL(/.*\/home/)
+    // Generic Assertions
+    const actualText = await page.getByText('Single Click Me').textContent()
+    expect(actualText).toEqual('Single Click Me');
+    // Locator Assertion
+    const singleClickButton = page.getByText('Single Click Me');
+    await expect(singleClickButton).toBeVisible();
+    await expect(singleClickButton).toBeEnabled();
   });
