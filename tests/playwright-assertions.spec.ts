4074620b84ffed989447c3ee800000b0ee42fb70
Animesh66 <er.animesh6@hotmail.com>
2026-02-11 20:53:10 +1100

    Updated the test with correct details.

--- tests/playwright-assertions.spec.ts
+++ tests/playwright-assertions.spec.ts
@@ -1,23 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 
-  test('Use Assertions in Playwright', async ({ page }) => {
+  test('Playwright Wait, resolve synchronization issue', async ({ page }) => {
     await page.goto('http://localhost:5173/');
-    // Page Assertion
     await expect(page).toHaveTitle(/learnwithanimesh/);
     await expect(page).toHaveURL(/.*\/home/)
-    // Generic Assertions
-    const actualText = await page.getByText('Single Click Me').textContent()
-    expect(actualText).toEqual('Single Click Me');
-    // Locator Assertion
-    const singleClickButton = page.getByText('Single Click Me');
-    await expect(singleClickButton).toBeVisible();
-    await expect(singleClickButton).toBeEnabled();
-    // Negative Assertions
-    await expect(singleClickButton).not.toBeDisabled();
-    // Soft Assertions
-    await expect.soft(singleClickButton).toBeDisabled();
-    // Assertion  after soft assertion
-    await expect(page.getByText('Double Click Me')).toBeVisible();
-    await expect(page.getByText('Double Click Me')).toBeDisabled();
+    // Click on the submit button
+    await page.getByText('Submit Form').click()
+    // Static Wait
+    await page.waitForTimeout(10000);
+    await expect(page.locator('#success-msg')).toHaveText('Form Submitted successfully');
+    // Dynamic Wait
+    await page.locator('#delayed-msg').waitFor();
+    const expectedText = await page.locator('#success-msg').textContent();
+    expect(expectedText).toBe('Form Submitted successfully');
+    //Assertion Wait
+    await expect(page.locator('#success-msg')).toHaveText('Form Submitted successfully', { timeout: 10000 });
+    
   });
