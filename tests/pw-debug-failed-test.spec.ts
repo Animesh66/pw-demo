aab642c4b25253a3f64ee18166bef264367ee5ee
Animesh66 <er.animesh6@hotmail.com>
2026-02-17 20:15:48 +1100

    Updated the test with correct details.

---
+++ tests/pw-debug-failed-test.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+
+  test('This test will fail, debug and find the root cause', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await expect(page).toHaveTitle(/learnwithanimesh/);
+    await expect(page).toHaveURL(/.*\/home/)
+    // Click on the submit button
+    await page.getByText('Submit Form').click()
+    // Verify the success message
+    await expect(page.locator('#success-msg')).toHaveText('Form Submitted successfully');
+    //Close the page at the end of the test
+    await page.close();
+  });
+
+
